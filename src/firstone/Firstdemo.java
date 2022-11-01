f292b5d9f1dcc5e369555cb717ad8bb89b051854
APARANJI <APARANJI@LAPTOP-NVIS5OT0>
2022-11-01 15:18:04 +0530

    first commit

--- src/firstone/Firstdemo.java
+++ src/firstone/Firstdemo.java
@@ -4,7 +4,7 @@ public class Firstdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world from dp");
 	}
 
 }
